82322614a8314649afc7facd33001f97534a3aee
MemoryDxx <1350564773@qq.com>
2018-04-23 23:09:13 +0800

    update

--- application/login.php
+++ application/login.php
@@ -1,22 +1,24 @@
 <?php
-    $con = mysql_connect("localhost","ubuntu","");
-    if (!$con)
+    $servername = "localhost";
+    $username = "ubuntu";
+    $password = "Aq3*l@7QFg";
+    $bdname = "graduation";
+    $conn = mysqli_connect($servername, $username, $password, $bdname);
+    if (!$conn)
       {
         echo "error";
-        die('Could not connect: ' . mysql_error());
+        die('Could not connect: ' . mysqli_connect_error());
       }
-    
-    mysql_select_db("graduation", $con);
-    
+        
     $sql="INSERT INTO user (user_name, user_email, user_phone, user_pwd)
     VALUES
     ('$_POST[user_name]','$_POST[user_email]','$_POST[user_phone]','$_POST[user_pwd]')";
     
-    if (!mysql_query($sql,$con))
-      {
-      die('Error: ' . mysql_error());
-      }
-    echo "1 record added";
+    if ($conn->query($sql) === TRUE){
+      echo "success";
+    }else{
+      echo "error: " . $sql . "<br>" . $conn->error;
+    }
     
-    mysql_close($con)
+    $conn->close();
 ?>
