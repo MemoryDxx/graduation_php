fef0e9e17970f7a36bfb003dfd6fc9bb0919a354
root <root@localhost.localdomain>
2018-04-23 23:47:48 +0800

    update

--- application/login.php
+++ application/login.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
-    $username = "ubuntu";
-    $password = "kangxinyuan";
+    $username = "root";
+    $password = "mysql";
     $bdname = "graduation";
     $conn = mysqli_connect($servername, $username, $password, $bdname);
     if (!$conn)
