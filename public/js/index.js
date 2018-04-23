07ce4ab5348d21522b226f1522340483049e55bd
MemoryDxx <nightcat.dxx@foxmail.com>
2018-04-23 21:45:12 +0800

    注册登录相关js

---
+++ public/js/index.js
@@ -0,0 +1,74 @@
+// 点击注册登录
+function lrbtn() {
+    var logwindow = document.getElementsByClassName("reg-login-panel");
+    logwindow[0].style.display = "block";
+}
+
+// 关闭注册登录面板
+function closepop() {
+    var popup = document.getElementsByClassName("reg-login-panel");
+    popup[0].style.display = "none";
+}
+
+// 登录切换至注册
+function swlog() {
+    var navtab = document.getElementById("lognav");
+    var parent = navtab.parentNode;
+    var panel = document.getElementById("login-form");
+    var panel1 = document.getElementById("reg-form");
+    navtab.className = "active";
+    parent.childNodes[3].className = "";
+    panel.style.display = "block";
+    panel1.style.display = "none";
+}
+
+// 注册切换至登录
+function swreg() {
+    var navtab = document.getElementById("regnav");
+    var parent = navtab.parentNode;
+    var panel = document.getElementById("login-form");
+    var panel1 = document.getElementById("reg-form");
+    navtab.className = "active";
+    parent.childNodes[1].className = "";
+    panel.style.display = "none";
+    panel1.style.display = "block";
+}
+
+// 数据提交
+function datapost(data,url) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        // readystate为4，请求已完成
+        if (xhr.readyState ==4) {
+            if((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304){
+                // 解析请求返回的JSON数据
+                var data = JSON.parse(xhr.responseText);
+            }
+        }
+    };
+    xhr.open("post",url,true);
+    xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xhr.send(data);
+}
+
+// 注册
+function login() {
+    var user = new Object();
+    user.user_name = document.getElementById("inputUsername3").value;
+    user.user_phone = document.getElementById("inputPhone3").value;
+    user.user_email = document.getElementById("inputEmail3").value;
+    user.user_pwd = document.getElementById("inputPassword4").value;
+    // 序列化参数
+    var data = JSON.stringify(user);
+    datapost(data,"http://123.207.141.123/application/login.php");
+}
+
+// 登录
+function reg() {
+    var user = new Object();
+    user.user_name = document.getElementById("inputUsername3").value;
+    user.user_pwd = document.getElementById("inputPassword3").value;
+    // 序列化参数
+    var data = JSON.stringify(user);
+    datapost(data,"http://123.207.141.123/application/reg.php");
+}
