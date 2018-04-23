e4f7b63124fada10ba5b45e54bd155e6bed24dea
MemoryDxx <1350564773@qq.com>
2018-04-23 22:47:21 +0800

    update

--- application/login.php
+++ application/login.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysql_connect("localhost","ubuntu","Aq3*l@7QFg");
+    $con = mysql_connect("localhost","root","kangxinyuan");
     if (!$con)
       {
       die('Could not connect: ' . mysql_error());
