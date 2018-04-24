81866a7404f9e2e54688775fb14e16e38f145e79
MemoryDxx <1350564773@qq.com>
2018-04-24 09:23:31 +0800

    update

--- public/js/index.js
+++ public/js/index.js
@@ -62,7 +62,7 @@ function login() {
     user.user_pwd = document.getElementById("inputPassword3").value;
     // 序列化参数
     var data = JSON.stringify(user);
-    datapost(user,"http://123.207.141.123/application/login.php");
+    datapost(data,"http://123.207.141.123/application/login.php");
 }
 
 // 登录
