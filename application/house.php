84462ca11b46134104eada9f415fc77ab0c19ccf
MemoryDxx <kangxinyuan@sina.cn>
2018-04-30 18:58:23 +0800

    update

--- application/house.php
+++ application/house.php
@@ -12,7 +12,7 @@
     $hid = $_POST['hid'];
     $res = $conn->query("SELECT * FROM house WHERE house_id='$hid'");
     $row = mysqli_fetch_assoc($res);
-    $name = $res['uname'];
+    $name = $row['uname'];
     // $res1 = $conn->query("SELECT uphone FROM user WHERE uname='$name'");
     // $row1 = mysqli_fetch_assoc($res1);
     // $row['uphone'] = $res1['uphone'];
