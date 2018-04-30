100c5a04d7b54ac23f97d8cef936e4aed744bef8
MemoryDxx <kangxinyuan@sina.cn>
2018-04-30 18:50:56 +0800

    update

--- application/house.php
+++ application/house.php
@@ -15,8 +15,9 @@
     $name = $res['uname'];
     $res1 = $conn->query("SELECT uphone FROM user WHERE uname='$name'");
     $row1 = mysqli_fetch_assoc($res1);
-    $row['uphone'] = $row1['uphone'];
+    $row['uphone'] = $res1['uphone'];
     
+    echo $row1['uphone'];
     echo json_encode($row);
 
     $conn->close();
