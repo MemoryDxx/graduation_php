66b0e87bd8b6a4c19827c499013ab45d7cff24ea
MemoryDxx <kangxinyuan@sina.cn>
2018-05-05 00:35:37 +0800

    update

--- application/housepage.php
+++ application/housepage.php
@@ -12,14 +12,18 @@
     $start = $_POST['start'];
     $page = $start;
     $inde = ($page-1)*5;
+    // 尝试
     $cou = $conn->query("SELECT COUNT(*) FROM house");
     $res1 = $conn->query("SELECT * FROM house LIMIT {$inde},5");
     // $res1 = $conn->query("SELECT * FROM house LIMIT {$inde},5");  顺序取数据
     // if($inde == 0){
     //     $res1 = $conn->query("SELECT * FROM house LIMIT {$inde},5");
     // }
+
+    // 取出全部数据逆序
+    $res2 = $conn->query("SELECT * FROM house");
     $result = array();
-    while ($row1 = mysqli_fetch_assoc($res1)) {
+    while ($row1 = mysqli_fetch_assoc($res2)) {
         # code...
         $result[] = $row1;
     }
